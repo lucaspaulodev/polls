e4ca6efab83e505c8e81d58aa17cb0733b5e048e
Lucas Paulo <lucaspaulodev@gmail.com>
2024-02-08 13:14:19 -0300

    Vote on poll logic, model and implementation.

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,20 @@
 import fastify from 'fastify'
 import { createPoll } from './routes/create-poll';
 import { getPoll } from './routes/get-poll';
+import { vateOnPoll } from './routes/vote-on-poll';
+import cookie from "@fastify/cookie"
 
 const app = fastify()
 
+app.register(cookie, {
+    secret: "polls-app-uahsu",
+    hook: "onRequest",
+    parseOptions: {}
+})
+
 app.register(getPoll)
 app.register(createPoll)
+app.register(vateOnPoll)
 
 app.listen({port: 3333}).then(() => {
     console.log('HTTP server is running...')
