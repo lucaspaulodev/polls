697d6fa70556f91d62d923901ffe15f46f4b6b69
Lucas Paulo <lucaspaulodev@gmail.com>
2024-02-14 00:18:11 -0300

    Creating Websocket connection.

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,6 +3,8 @@ import { createPoll } from './routes/create-poll';
 import { getPoll } from './routes/get-poll';
 import { vateOnPoll } from './routes/vote-on-poll';
 import cookie from "@fastify/cookie"
+import websocket from '@fastify/websocket'
+import { pollResults } from './ws/poll-results';
 
 const app = fastify()
 
@@ -12,9 +14,13 @@ app.register(cookie, {
     parseOptions: {}
 })
 
+app.register(websocket)
+
 app.register(getPoll)
 app.register(createPoll)
 app.register(vateOnPoll)
+
+app.register(pollResults)
 
 app.listen({port: 3333}).then(() => {
     console.log('HTTP server is running...')
