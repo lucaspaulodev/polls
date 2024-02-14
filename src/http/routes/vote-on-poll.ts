e50a87de50521f8875a0dcceb4bab69eae0a8782
Lucas Paulo <lucaspaulodev@gmail.com>
2024-02-14 19:05:54 -0300

    Applying Pub Sub pattern.

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -3,6 +3,7 @@ import { FastifyInstance } from 'fastify'
 import { randomUUID } from 'crypto'
 import { prisma } from '../../lib/prisma'
 import { redis } from '../../lib/redis'
+import { voting } from '../../utils/voting-pub-sub'
 
 export async function vateOnPoll(app: FastifyInstance) {
     app.post("/polls/:pollId/votes", async (request, reply) => {
@@ -35,7 +36,12 @@ export async function vateOnPoll(app: FastifyInstance) {
                     }
                 })
 
-                await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
+                const votes = await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
+                
+                voting.publish(pollId, {
+                    pollOptionId: userPreviousVoteOnPoll.pollOptionId,
+                    votes: Number(votes),
+                })
             } else if(userPreviousVoteOnPoll) {
                 return reply.status(400).send({message: 'You already voted on this poll.'})
             }
@@ -60,7 +66,12 @@ export async function vateOnPoll(app: FastifyInstance) {
             }
         })
 
-        await redis.zincrby(pollId, 1, pollOptionId)
+        const votes =await redis.zincrby(pollId, 1, pollOptionId)
+
+        voting.publish(pollId, {
+            pollOptionId,
+            votes: Number(votes),
+        })
 
         return reply.status(201).send()
     })
